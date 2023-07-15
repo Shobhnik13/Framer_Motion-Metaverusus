6b35aea3c17973b6015494b1166faa1055328198
Shobhnik13 <shobhnikw@gmail.com>
2023-07-15 16:39:14 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="App bg-black overflow-hidden">
+     <Navbar/>
     </div>
   );
 }
