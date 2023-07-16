7a351d3150e661dd820161873ba83376f69207dd
Shobhnik13 <shobhnikw@gmail.com>
2023-07-16 16:01:28 +0530

    completed hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Hero from './Components/Hero';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App bg-black overflow-hidden">
      <Navbar/>
+     <Hero/>
     </div>
   );
 }
