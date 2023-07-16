5a82150acb4b326f7d9abe85d60e41abda70a5ac
Shobhnik13 <shobhnikw@gmail.com>
2023-07-17 02:09:14 +0530

    about section done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 
 import './App.css';
+import About from './Components/About';
+import Explore from './Components/Explore';
 import Hero from './Components/Hero';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App bg-black overflow-hidden">
-     <Navbar/>
-     <Hero/>
-    </div>
+<div className="App bg-black overflow-hidden">
+    <Navbar/>
+    <Hero/>
+    {/* positioning the components in relative  */}
+      <div className='relative'>
+        {/* writing down the comps and self closing div for rendering gradients  */}
+      <About/>
+        <div className='gradient-03 z-0'/>
+      <Explore/>
+      </div>
+</div>
   );
 }
 
